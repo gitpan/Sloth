8d10f201491c2cda109bd746b56088b291486209
Oliver Charles <oliver@ocharles.org.uk>
2011-02-25 13:32:56 +0000

    Import of CYCLES/Sloth-0.01 from CPAN.
    gitpan-cpan-distribution: Sloth
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         CYCLES/Sloth-0.01.tar.gz
    gitpan-cpan-author:       CYCLES
    gitpan-cpan-maturity:     released

---
+++ lib/Sloth/Request.pm
@@ -0,0 +1,42 @@
+package Sloth::Request;
+BEGIN {
+  $Sloth::Request::VERSION = '0.01';
+}
+use Moose;
+use namespace::autoclean;
+
+has plack_request => (
+    is => 'ro',
+    required => 1,
+    handles => [qw( path method query_parameters header )],
+);
+
+has path_components => (
+    required => 1,
+    is => 'ro'
+);
+
+1;
+
+__END__
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Sloth::Request
+
+=head1 AUTHOR
+
+Oliver Charles
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Oliver Charles <sloth.cpan@ocharles.org.uk>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ lib/Sloth/Resource.pm
@@ -0,0 +1,138 @@
+package Sloth::Resource;
+BEGIN {
+  $Sloth::Resource::VERSION = '0.01';
+}
+# ABSTRACT: A resource that exposed by the REST server
+
+use Moose::Role;
+use namespace::autoclean;
+
+use HTTP::Throwable::Factory 'http_throw';
+use Module::Pluggable::Object;
+use Plack::Response;
+
+
+has representations => (
+    required => 1,
+    isa => 'ArrayRef',
+    traits => [ 'Array' ],
+    handles => {
+        representations => 'elements'
+    }
+);
+
+
+has methods => (
+    isa => 'HashRef',
+    is => 'ro',
+    required => 1,
+    traits => [ 'Hash' ],
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        my $mpo = Module::Pluggable::Object->new(
+            search_path => $self->meta->name,
+            require => 1
+        );
+        return {
+            map {
+                my ($method) = $_ =~ /.*::([a-z]*)$/i;
+                uc($method) => $_->new
+            } grep {
+                $_->does('Sloth::Method')
+            } $mpo->plugins
+        }
+    },
+    handles => {
+        _method_handler => 'get',
+        supported_methods => 'keys'
+    }
+);
+
+sub _serializer {
+    my ($self, $type) = @_;
+    for my $rep ($self->representations) {
+        return $rep if $type =~ $rep->content_type;
+    }
+}
+
+
+sub handle_request {
+    my ($self, $request) = @_;
+
+    my $method = $self->_method_handler($request->method)
+        or return http_throw('MethodNotAllowed' => {
+            allow => [ $self->supported_methods ]
+        });
+
+    my $resource = $method->process_request($request);
+
+    my @accept = $request->header('Accept');
+    for my $accept ($request->header('Accept')) {
+        my $serializer = $self->_serializer($accept)
+            or next;
+
+        return Plack::Response->new(
+            200 => [] => $serializer->serialize($resource)
+        ) or http_throw('NotAcceptable');
+    }
+
+    http_throw('NotAcceptable');
+}
+
+1;
+
+__END__
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Sloth::Resource - A resource that exposed by the REST server
+
+=head1 ATTRIBUTES
+
+=head2 representations
+
+A C<ArrayRef[Sloth::Representation]> of all known representations of resources.
+
+By default, this will be taken from L<Sloth>, your main Sloth application.
+However, if this resource only has specific representations that differ from the
+rest of you application, you can override it.
+
+=head2 methods
+
+A C<Map[MethodName => Sloth::Method>.
+
+A map of allowed HTTP methods on this resource, to their L<Sloth::Method>
+implementation. By default you do not need to worry about specifying this
+attribute as Sloth will default to looking for methods below the current
+resource namespace (for example, C<Resource::Pancake> would look for
+C<Resource::Pancake::GET> and so on).
+
+=head1 METHODS
+
+=head2 handle_request
+
+    $self->handle_request($request : Sloth::Request)
+
+Handle a request for a resource.
+
+You will not normally need to change this method, as by default
+it will check if the method is allowed, if there is an available
+serializer, and handle all the dispatching for you.
+
+=head1 AUTHOR
+
+Oliver Charles
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Oliver Charles <sloth.cpan@ocharles.org.uk>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
